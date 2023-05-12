e545eb43e29b1cf8fd7ca795037d57202d9c09dd
Alexey <temnikovalx@icloud.com>
2023-05-12 22:57:08 +0300

    trying to push vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 import Main from "./pages/Main";
+import Home from "./pages/Home";
 
 function App() {
   return (
